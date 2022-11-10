ad2f54b7231f6e88d23c92f388a768dd6fe01bd9
Margauxd28 <26036@student.dorset-college.ie>
2022-11-10 11:00:33 +0100

    first commit

---
+++ Lecture5/Motorcycle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture5
+{
+    abstract class Motorcycle
+    {
+        
+        public void StartEngine() { }
+        protected void AddGas(int gallons) { }
+        double miles = 10;
+        double speed = 100;
+        public virtual int Drive(int miles, int speed) { return (int)Math.Round(((double)miles)/speed,0); }
+        public virtual int Drive(TimeSpan time, int speed) { return 0; }
+        public abstract double GetTopSpeed();
+    }
+    class SportsMotorcycle : Motorcycle
+    {
+        
+        public override double GetTopSpeed()
+        {
+            return 70.05;
+        }
+        double _weighRatio = 1.2;
+        public override int Drive(int miles, int speed)
+        {
+            return (int)Math.Round((double)miles / speed, 0) * (int)_weighRatio;
+        }
+    }
+    class SportsMotorCylcleWithSideCar : Motorcycle
+    {
+        float _weightRatio = 0.8f;
+        public override double GetTopSpeed()
+        {
+            return 70.05;
+        }
+        public override int Drive(int miles, int speed) { return base.Drive(miles, speed) * (int)_weightRatio; }
+    }
+}
---
+++ Lecture5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture5
+{
+    class Program
+    {
+        static string GetVowels(params string []input)
+        {
+            if(input==null||input.Length==0)
+            {
+                return string.Empty;
+            }
+            var vowels = new char[] { 'A', 'E', 'I', 'O', 'U' };
+            return string.Concat(input.SelectMany(word => word.Where(letter => vowels.Contains(char.ToUpper(letter)))));
+        }
+
+        static void Example1()
+        {
+            string fromArray = GetVowels(new[] { "apple", "banana", "pear" });
+            Console.WriteLine($"Vowels from array : '{fromArray}'");
+
+            string fromMultipleArguments = GetVowels("apple", "banana", "pear");
+            Console.WriteLine($"Vowels from multiple arguments :'{fromMultipleArguments}'");
+
+            string fromNull = GetVowels(null);
+            Console.WriteLine($"Vowels from null :'{fromNull}'");
+
+            string fromNoValue = GetVowels();
+            Console.WriteLine($"Vowels from no value : '{fromNoValue}'");
+        }
+        static void ModifyValue(int variable1)
+        {
+            variable1 = 30;
+            Console.WriteLine("In ModifyValue, variable1 = {0}", variable1);
+            return;
+        }
+        static void Example2()
+        {
+            int variable1 = 245 ;
+            Console.WriteLine("In Main, variable1 ={0}",variable1);
+            ModifyValue(variable1);
+            Console.WriteLine("Back in Main, variable1 ={0}", variable1);
+        }
+
+
+        
+        static void Main(string[] args)
+        {
+            //Example1();
+            //Example2();
+        }
+    }
+}
